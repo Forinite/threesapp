140ed15c555836ccba26e5ce7ef88e6fa0d7c883
Forinite <obefortune2@gmail.com>
2025-09-01 06:01:44 +0100

    Theme Colors Fixed

--- app/main/SideSection.tsx
+++ app/main/SideSection.tsx
@@ -5,7 +5,9 @@ import React from "react";
 const SideSection = ()=> {
     return (
         <div className="w-[25%] min-w-72 border-r border-gray-300 bg-gray-100 flex items-center justify-center">
-            <span className="text-xs text-gray-500">S2</span>
+            <div className="text-xs text-gray-500">
+               S2
+            </div>
         </div>
     );
 }
